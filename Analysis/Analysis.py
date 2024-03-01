df67d998ab37db446612f12eabb62548da80d5e8
karimyounus <younus.karim35@gmail.com>
2024-03-01 14:18:17 +0000

    Final commit

--- Analysis/Analysis.py
+++ Analysis/Analysis.py
@@ -12,26 +12,32 @@ DATASETS = (15000, 30000, 100000)
 CORE_COUNTS = [2, 4, 8, 16, 32, 64]
 SCHEDULING_STRATEGIES = ["static", "dynamic", "guided"]
 CHUNK_SIZES = [1, 10, 20, 40, 60, 80, 100, 120, 140, 160]
+GCD_VERSION = ["Euclid", "Binary"]
 path = "Benchmarks"
 
 
 def main():
     # Import CSV files
-    seq_df, para_df = import_csv(path)
+    seq_df, para_df = import_csv(path, False)
+    amd_df, intel_df = import_csv(path + "/Seq", True)
     euclid_seq_df = seq_df.loc[seq_df['GCD Version'] == 'Euclid']
+
     # COMPUTE UNIQUE MEAN EXECUTION TIME
     para_mean = compute_unique_mean_execution_time(para_df, COLUMNS)
     seq_mean = compute_unique_mean_execution_time(euclid_seq_df, COLUMNS)
+    amd_mean = compute_unique_mean_execution_time(amd_df, COLUMNS)
+    intel_mean = compute_unique_mean_execution_time(intel_df, COLUMNS)
     # COMPUTE SPEEDUP
     seq_mean_rt = seq_mean['Mean Execution Time'].unique()
     para_processed = compute_speedup(para_mean, seq_mean_rt)
     # COMPUTE EFFICIENCY
     para_processed['Efficiency'] = para_processed['Speedup'] / para_processed['Core Count']
 
-    # PLOT
+    # plot_speedup_vs_chunk_size(para_processed)
 
-    # Print the table string
-    print(generate_performance_table(para_df))
+    table = generate_performance_table(para_processed)
+    print(table)
+    # PLOT
 
 
 def plot_speedup_vs_chunk_size(processed_df):
@@ -40,28 +46,29 @@ def plot_speedup_vs_chunk_size(processed_df):
     :param processed_df:
     :return:
     """
-    ds1 = select_combinations(processed_df, upper=15000, scheduling_strategy='guided', core_count=4)
-    ds2 = select_combinations(processed_df, upper=30000, scheduling_strategy='guided', core_count=4)
-    ds3 = select_combinations(processed_df, upper=100000, scheduling_strategy='guided', core_count=4)
+    ds1 = select_combinations(processed_df, upper=100000, scheduling_strategy='static', core_count=4)
+    ds2 = select_combinations(processed_df, upper=100000, scheduling_strategy='guided', core_count=4)
+    ds3 = select_combinations(processed_df, upper=100000, scheduling_strategy='dynamic', core_count=4)
 
     plt.figure(figsize=(10, 6))
-    plt.plot(ds3['Chunk Size'], ds3['Speedup'], label='DS3')
-    plt.plot(ds2['Chunk Size'], ds2['Speedup'], label='DS2')
-    plt.plot(ds1['Chunk Size'], ds1['Speedup'], label='DS1')
+    plt.plot(ds3['Chunk Size'], ds3['Speedup'], label='Dynamic')
+    plt.plot(ds2['Chunk Size'], ds2['Speedup'], label='Guided')
+    plt.plot(ds1['Chunk Size'], ds1['Speedup'], label='Static')
     plt.legend()
     plt.xlabel('Chunk Size')
     plt.ylabel('Speedup Factor')
-    plt.title('Speedup vs Chunk Size')
+    plt.title('Speedup vs Chunk Size for Scheduling Strategies, Core Count = 4, DS3')
     plt.show()
 
-def plot_main_graph(processed_df):
+
+def plot_main_graph_speedup(processed_df):
     """
     Plot the speedup vs chunk size against the core count for each dataset
     :param processed_df:
     :return:
     """
 
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6), sharey=True)  # Share the y-axis among all subplots
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 18))  # Share the y-axis among all subplots
 
     for i, upper in enumerate(DATASETS):
         # Filter data for each subplot
@@ -90,19 +97,22 @@ def plot_main_graph(processed_df):
                                      fontsize=10,
                                      bbox=dict(boxstyle='round,pad=0.01', fc='white', alpha=0.3))
 
+        # Add Ideal speedup line
+        axes[i].plot(CORE_COUNTS, CORE_COUNTS, label='Ideal Speedup', linestyle='--', color='gray')
+
         # Customizing the subplot
-        axes[i].set_title(f'DS{i + 1}')
+        axes[i].set_title(f'DS{i + 1}', fontsize=16)
         axes[i].set_xlabel('Core Count')
         axes[i].grid(True)
-        if i == 0:
-            axes[i].set_ylabel('Speedup')
+        axes[i].set_ylabel('Speedup')
         axes[i].legend()
 
-    plt.suptitle('Parallel vs Sequential Speedup across Core Count', fontsize=16)
+    plt.suptitle('Parallel vs Sequential Speedup across Core Count\n', fontsize=16)
     plt.tight_layout()
     plt.show()
 
     return fig
+
 
 def plot_main_graph_runtime(processed_df):
     """
@@ -111,7 +121,7 @@ def plot_main_graph_runtime(processed_df):
     :return:
     """
 
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 18))
 
     for i, upper in enumerate(DATASETS):
         # Filter data for each subplot
@@ -141,18 +151,18 @@ def plot_main_graph_runtime(processed_df):
                                      bbox=dict(boxstyle='round,pad=0.01', fc='white', alpha=0.3))
 
         # Customizing the subplot
-        axes[i].set_title(f'DS{i + 1}')
-        axes[i].set_xlabel('Core Count')
+        axes[i].set_title(f'DS{i + 1}', fontsize=16)
+        axes[i].set_xlabel('Core Count', fontsize=12)
         axes[i].grid(True)
-        if i == 0:
-            axes[i].set_ylabel('Mean Execution Time (s)')
+        axes[i].set_ylabel('Mean Execution Time (s)', fontsize=12)
         axes[i].legend()
 
-    plt.suptitle('Parallel Mean Execution Time across Core Count', fontsize=16)
+    plt.suptitle('Parallel Mean Execution Time across Core Count\n', fontsize=16)
     plt.tight_layout()
     plt.show()
 
     return fig
+
 
 def plot_main_graph_efficiency(processed_df):
     """
@@ -161,11 +171,11 @@ def plot_main_graph_efficiency(processed_df):
     :return:
     """
 
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 18), sharey=True)
 
     for i, upper in enumerate(DATASETS):
         # Filter data for each subplot
-        ds = select_combinations(processed_df, upper=upper, scheduling_strategy='guided')
+        ds = select_combinations(processed_df, upper=upper, scheduling_strategy='dynamic')
 
         # Plot each chunk size
         for chunk_size in CHUNK_SIZES:
@@ -173,14 +183,13 @@ def plot_main_graph_efficiency(processed_df):
             axes[i].plot(subset['Core Count'], subset['Efficiency'], label=f'Chunk size = {chunk_size}')
 
         # Customizing the subplot
-        axes[i].set_title(f'DS{i + 1}')
+        axes[i].set_title(f'DS{i + 1}', fontsize=16)
         axes[i].set_xlabel('Core Count')
         axes[i].grid(True)
-        if i == 0:
-            axes[i].set_ylabel('Efficiency')
+        axes[i].set_ylabel('Efficiency')
         axes[i].legend()
 
-    plt.suptitle('Parallel Efficiency across Core Count', fontsize=16)
+    plt.suptitle('Parallel Efficiency across Core Count\n', fontsize=16)
     plt.tight_layout()
     plt.show()
 
@@ -220,6 +229,32 @@ def plot_main_graph_sched(processed_df):
     return fig
 
 
+def plot_amd_vs_intel_runtime(amd, intel):
+    """
+    Plot the speedup vs core count for each dataset
+    :param processed_df:
+    :return:
+    """
+    plt.figure(figsize=(10, 6))
+
+    for i, gcd in enumerate(GCD_VERSION):
+        # Filter data for each subplot
+        ds_amd = select_combinations(amd, gcd_version=gcd)
+        ds_intel = select_combinations(intel, gcd_version=gcd)
+
+        plt.plot(ds_amd['Upper'], ds_amd['Mean Execution Time'], label=f'AMD, GCD = {gcd}')
+        plt.plot(ds_intel['Upper'], ds_intel['Mean Execution Time'], label=f'Intel, GCD = {gcd}')
+
+    plt.title('Mean Execution Time vs. Upper Bound')
+    plt.xlabel('Upper Bound')
+    plt.grid(True)
+    plt.ylabel('Mean Execution Time (s)')
+    plt.legend()
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+    plt.show()
+
 
 def plot_speedup_vs_core_count(processed_df):
     """
@@ -241,21 +276,6 @@ def plot_speedup_vs_core_count(processed_df):
     plt.title('Speedup vs Core Count')
     plt.show()
 
-def plot_speedup_vs_chunk_size(processed_df):
-
-    ds1 = select_combinations(processed_df, upper=15000, scheduling_strategy='dynamic', core_count=32)
-    ds2 = select_combinations(processed_df, upper=30000, scheduling_strategy='dynamic', core_count=32)
-    ds3 = select_combinations(processed_df, upper=100000, scheduling_strategy='dynamic', core_count=32)
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(ds3['Chunk Size'], ds3['Speedup'], label='DS3')
-    plt.plot(ds2['Chunk Size'], ds2['Speedup'], label='DS2')
-    plt.plot(ds1['Chunk Size'], ds1['Speedup'], label='DS1')
-    plt.legend()
-    plt.xlabel('Chunk Size')
-    plt.ylabel('Speedup Factor')
-    plt.title('Speedup vs Chunk Size')
-    plt.show()
 
 def plot_scheduling_strategy(processed_df):
     """
@@ -288,22 +308,23 @@ def generate_performance_table(df):
 
     """
 
-    df_sorted = df.sort_values(by=['Core Count', 'Upper', 'Execution Time'])
+    df_sorted = df.sort_values(by=['Core Count', 'Upper', 'Mean Execution Time'])
 
     # Group the data by 'Core Count' and 'Dataset Size' and calculate the required statistics
     performance_stats = df_sorted.groupby(['Core Count', 'Upper']).agg(
-        First_Runtime=('Execution Time', lambda x: x.iloc[0] if len(x) > 0 else None),
-        Second_Runtime=('Execution Time', lambda x: x.iloc[1] if len(x) > 1 else None),
-        Third_Runtime=('Execution Time', lambda x: x.iloc[2] if len(x) > 2 else None),
-        Median_Runtime=('Execution Time', 'median'),
-        Mean_Runtime=('Execution Time', 'mean'),
-        Min_Runtime=('Execution Time', 'min'),
-        Max_Runtime=('Execution Time', 'max')
+        # First_Runtime=('Execution Time', lambda x: x.iloc[0] if len(x) > 0 else None),
+        # Second_Runtime=('Execution Time', lambda x: x.iloc[1] if len(x) > 1 else None),
+        # Third_Runtime=('Execution Time', lambda x: x.iloc[2] if len(x) > 2 else None),
+        # Median_Runtime=('Execution Time', 'median'),
+        # Mean_Runtime=('Execution Time', 'mean'),
+        # Min_Runtime=('Execution Time', 'min'),
+        # Max_Runtime=('Execution Time', 'max'),
+        Max_Speedup=('Speedup', 'max'),
     ).reset_index()
 
     # Calculate the differences between the mean and extreme values
-    performance_stats['Mean-Min Difference'] = performance_stats['Mean_Runtime'] - performance_stats['Min_Runtime']
-    performance_stats['Mean-Max Difference'] = performance_stats['Max_Runtime'] - performance_stats['Mean_Runtime']
+    # performance_stats['Mean-Min Difference'] = performance_stats['Mean_Runtime'] - performance_stats['Min_Runtime']
+    # performance_stats['Mean-Max Difference'] = performance_stats['Max_Runtime'] - performance_stats['Mean_Runtime']
 
     # Convert time to a suitable unit, e.g., milliseconds, for readability
     # Round the values for better readability
@@ -392,31 +413,39 @@ def select_combinations(df, upper=None, core_count=None, scheduling_strategy=Non
     return filtered_df
 
 
-def import_csv(path: str):
+def import_csv(csv_path: str, seq=False):
     def clean_column_names(df):
         # Strip whitespace and remove quotation marks from column names
         df.columns = df.columns.str.strip().str.replace('"', '').str.replace('\'', '')
         return df
 
     def clean_strings(df):
+        # Strip whitespace and remove quotation marks from strings
         df = df.map(lambda x: x.strip().replace('"', '').replace('\'', '') if isinstance(x, str) else x)
         return df
 
-    files = os.listdir(path)
+    files = os.listdir(csv_path)
     seq_df = []
     para_df = []
+
+    seq_pattern = ""
+    para_pattern = ""
 
     # regex
     seq_pattern = r"seq_benchmark_ds(\d+)_gcd(\d+).csv"
     para_pattern = r"para_benchmark_ds(\d+).csv"
 
+    if seq:
+        seq_pattern = r"seq_amd.csv"
+        para_pattern = r"seq_intel.csv"
+
     for file in files:
         if file.endswith(".csv"):
             if re.match(seq_pattern, file):
-                seq_df.append(pd.read_csv(os.path.join(path, file), dtype={'Scheduling Strategy': str}))
+                seq_df.append(pd.read_csv(os.path.join(csv_path, file), dtype={'Scheduling Strategy': str}))
                 # print("Added Seq")
             elif re.match(para_pattern, file):
-                para_df.append(pd.read_csv(os.path.join(path, file)))
+                para_df.append(pd.read_csv(os.path.join(csv_path, file)))
                 # print("Added Para")
             else:
                 print(f"Unknown file format: {file}")
